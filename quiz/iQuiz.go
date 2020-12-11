77dfaba622d9882867d226e2680e56756d3b7448
bpike0612 <pike.brett0809@gmail.com>
2020-12-10 22:39:34 -0500

    Builder pattern implementation
    Wanted to practice with the builder pattern.

--- quiz/iQuiz.go
+++
@@ -1,7 +0,0 @@
-package quiz
-
-type iQuiz interface {
-	PrepareQuiz(file string) ([]string, error)
-	TakeQuiz(map[string]string) (float64, float64)
-	Grade(correct float64, total float64, passingScore int) (bool, float64)
-}
